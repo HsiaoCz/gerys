3277acdb99c8c9273c9738f3e79fc063ddd5f672
HsiaoCz <984274788@qq.com>
2024-08-10 00:12:54 +0800

    add some

---
+++ src/underwood/03_events_bus/child2.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="child2">
+        <h2>我是子组件2</h2>
+        <button @click="handler">点击我给我兄弟送一台法拉利</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+// 引入$bus对象
+import $bus from '../../bus';
+// 点击按钮回调
+const handler = () => {
+    $bus.emit('car', { car: "法拉利" })
+}
+</script>
+
+<style scoped>
+.child {
+    width: 300px;
+    height: 200px;
+    background-color: blue;
+}
+</style>
