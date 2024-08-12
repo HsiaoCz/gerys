c524be9c6c0d42031ef73add501939e54969ad54
HsiaoCz <984274788@qq.com>
2024-08-12 08:50:13 +0800

    add some

---
+++ src/underwood/07_provide_inject/GrandChild.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="child1">
+        <h1>grandson components</h1>
+        <h1>{{ car }}</h1>
+        <button @click="updateCar"></button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+
+// 注入祖先组件提供数据
+// 需要参数：即为祖先提供数据的key
+let car = inject("TOKEN")
+console.log(car);
+
+const updateCar = () => {
+    // car.value="自行车"
+    car = "自行车"
+}
+
+</script>
+
+<style scoped>
+.child1 {
+    width: 200px;
+    height: 200px;
+    background: red;
+}
+</style>
---
+++ src/underwood/07_provide_inject/ProvideInjectTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="box">
+        <h1>Provide与Inject</h1>
+        <hr>
+        <child></child>
+    </div>
+</template>
+
+<script setup lang="ts">
+import child from './Child.vue';
+import { provide, ref } from "vue";
+
+// vue3 提供provide(提供)与inject(注入)，可以实现隔辈儿组件传递数据
+let car = ref("法拉利")
+
+// provide 是一个方法
+// 第一个参数就是提供的数据 的Key
+// 第二个参数：祖先组件提供的数据
+provide("TOKEN", car)
+</script>
+
+
+<style scoped>
+.box {
+    width: 100vw;
+    height: 600px;
+    background-color: skyblue;
+}
+</style>
