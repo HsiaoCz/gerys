ea109ff3d31d6d24b0b7422daf2b74ef4719b43c
HsiaoCz <984274788@qq.com>
2024-08-12 08:32:11 +0800

    add some

---
+++ src/underwood/06_ref_parent/RefChildrenParent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="box">
+        <h1>
+            this is parent {{ money }}
+        </h1>
+        <button @click="handler">dept from som component</button>
+        <Son ref="son"></Son>
+        <hr>
+        <Dau></Dau>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// ref:可以获取真实的DOM节点，可以获取到子组件实例VC
+// $parent 可以在子组件内部获取父组件的实例
+
+// 父组件
+let money = ref(1000000);
+// 子组件
+import Son from "./Son.vue";
+import Dau from "./Daughter.vue";
+
+// 获取子组件的实例
+let son = ref()
+
+// 父组件内部的实例
+const handler = () => {
+    money.value + 10
+    son.value.money -= 10
+    son.value.fly()
+}
+
+// 对外暴露父组件的数据
+defineExpose({
+    money,
+})
+</script>
+<style scoped>
+.box {
+    width: 100vw;
+    height: 500px;
+    background-color: pink;
+}
+</style>
---
+++ src/underwood/06_ref_parent/Son.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="son">
+        <h1>this is son components {{ money }}</h1>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+let money = ref(10000)
+
+const fly = () => {
+    console.log("我可以飞")
+}
+// 组件内部数据对外关闭，别人不能访问
+// 如果想让外部访问需要通过defineExpose方法对外暴露
+
+defineExpose({
+    money,
+    fly,
+})
+</script>
+
+<style scoped>
+.son {
+    width: 300px;
+    height: 500px;
+    background-color: skyblue;
+}
+</style>
