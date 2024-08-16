84f558a10a68e7098a4c206e547cb6cfcedff8d1
HsiaoCz <984274788@qq.com>
2024-08-16 22:16:41 +0800

    add some

---
+++ src/santino/01_props/child.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="son">
+        <h1>I am child component : Hello</h1>
+        <p>{{ props.info }}</p>
+        <p>{{ props.money }}</p>
+        <!-- props 可以省略名字 -->
+        <p>{{ info }}</p>
+        <p>{{ money }}</p>
+
+        <!-- props的数据是只读的 -->
+        <button @click="updateProps">修改props数据</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+// 组合式API
+// 需要使用defineProps方法去接收父组件传递过来的数据
+// defineProps是vue3提供的方法，不需要引入直接使用
+let props = defineProps(["info", "money"])
+console.log(props)
+
+// 按钮点击回调
+const updateProps = () => {
+    // props.money+=1
+    // props 的数据是只读的
+    console.log(props.info)
+    console.log(props.money)
+}
+
+</script>
+
+<style>
+.son {
+    width: 400px;
+    height: 200px;
+    background-color: hotpink;
+}
+</style>
---
+++ src/santino/01_props/props_test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box">
+        <h1>props : 我是父组件</h1>
+        <hr>
+        <Child info="hello" :money="money"></Child>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+// props可以实现父子组件通信，props数据是只读
+import { ref } from "vue";
+import Child from "./child.vue";
+
+let money = ref(10000);
+</script>
+
+
+<style scoped>
+.box {
+    width: 100vw;
+    height: 400px;
+    background-color: yellowgreen;
+}
+</style>
