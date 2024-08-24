810344c207782f15155583ead48885fac2d90170
HsiaoCz <984274788@qq.com>
2024-08-24 20:53:00 +0800

    add some

---
+++ src/golden/07_provide_inject/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="child">
+    <h1>我是子组件1</h1>
+    <GrandChild></GrandChild>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GrandChild from "./GrandChild.vue";
+</script>
+
+<style scoped>
+.child {
+  width: 300px;
+  height: 400px;
+  background: yellowgreen;
+}
+</style>
+
