a407052f145766b0f26aaf5c1f3cc858f6759eb4
HsiaoCz <984274788@qq.com>
2024-09-03 20:57:05 +0800

    add some

---
+++ src/smoogy/08_pinia/Child1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="child1">
+        <p>{{ userInfo.count }}</p>
+        <br>
+        <p @click="updateTodo">{{ todoStore.todos }},{{ todoStore.total }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import useInfoStore from '../../store/module/info';
+
+// 获取仓库数据
+let userInfo = useInfoStore();
+console.log(userInfo.count)
+
+// 引入组合式API函数仓库
+import useTodoStore from '../../store/module/todo';
+let todoStore = useTodoStore();
+
+// 修改todo数据
+const updateTodo = () => {
+    todoStore.updateTodo();
+}
+</script>
+
+<style>
+.child1 {
+    width: 200px;
+    height: 200px;
+    background-color: bisque;
+}
+</style>
