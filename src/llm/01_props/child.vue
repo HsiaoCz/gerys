4af01c277c9876d9dafadb0ff409ff70a57bcf7b
HsiaoCz <984274788@qq.com>
2024-09-04 06:03:21 +0800

    add some

---
+++ src/llm/01_props/child.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// 组合式API
+// 需要使用defineProps方法去接收父组件传递过来的数据
+// defineProps是vue3提供的方法，不需要引入直接使用
+let props = defineProps(['info', 'money'])
+
+// 按钮点击的回调
+const updateProps = () => {
+    // props数据只读
+    console.log(props.info)
+    console.log(props.money)
+}
+</script>
+
+<template>
+    <div class="son">
+        <h1>我是子组件:hello</h1>
+        <p>{{ props.info }}</p>
+        <p>{{ props.money }}</p>
+        <!-- props可以省略前面的名字 -->
+        <p>{{ info }}</p>
+        <p>{{ money }}</p>
+        <!-- props的数据是只读的 -->
+        <button @click="updateProps">修改props数据</button>
+    </div>
+</template>
+
+
+<style scoped>
+.son {
+    width: 400px;
+    height: 200px;
+    background-color: hotpink;
+}
+</style>
