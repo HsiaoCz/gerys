1cf4b061012b45d5a2a876bb4485c16b74ed2861
HsiaoCz <984274788@qq.com>
2024-09-07 00:05:37 +0800

    add some

---
+++ src/llm/09_slot/SlotTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Slot</h1>
+        <Test>
+            <!-- 匿名插槽 -->
+            <div>
+                <pre>大江东去</pre>
+            </div>
+            <!-- 具名插槽 -->
+            <template v-slot:abc>
+                <div>填充具名插槽</div>
+            </template>
+
+            <template v-slot:efg>
+                <div>填充匿名插槽</div>
+            </template>
+        </Test>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import Test from './Test.vue';
+
+// 插槽:默认插槽、具名插槽、作用域插槽
+
+</script>
+
+<style scoped></style>
