f2451cb2c8495b4fbefcac5adf5726205a4c7970
HsiaoCz <984274788@qq.com>
2024-09-15 04:46:20 +0800

    add some

---
+++ src/jojo/03_event-bus/child1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="child1">
+        <h3>我是子组件1:Hello</h3>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.child {
+    width: 300px;
+    height: 300px;
+    background: rebeccapurple;
+}
+</style>
