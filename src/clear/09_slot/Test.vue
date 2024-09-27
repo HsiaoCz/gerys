994d210b6444263b8f65f33cf31f37b7ea72fe5d
HsiaoCz <984274788@qq.com>
2024-09-27 20:35:55 +0800

    add some

---
+++ src/clear/09_slot/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box">
+        <h1>我是默认插槽</h1>
+        <!-- 默认插槽 -->
+        <slot></slot>
+        <h1>我是子组件默认插槽</h1>
+        <h1>具名插槽填充数据</h1>
+        <slot name="abc"></slot>
+        <h1>具名插槽填充数据</h1>
+        <slot name="efg"></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.box {
+    width: 100px;
+    height: 200px;
+    background-color: skyblue;
+}
+</style>
